6ff3fbcf036799ba7fe54dba3931109ad0d54da6
Anna Reznikova <reznic11@rambler.ru>
2013-04-13 17:58:04 +0400

    notebook1
    problem with Tests

---
+++ Create_Test.java
@@ -0,0 +1,45 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Анна
+ * Date: 13.04.13
+ * Time: 6:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Create_Test {
+    @Test  //если вызваем команду open 2 раза, открывает файл тоже 2 раза
+    public void doubleOpen() throws Exception {
+        final NoteBook  c = new NoteBook ();
+        c.Open();
+        c.Open();
+    }
+
+    @Test  // вводим имя или номер, который уже есть в файле. Ошибка.
+    public void AddTheSame() throws Exception {
+        final NoteBook  c = new NoteBook ();
+        c.Add();
+
+    }
+
+    @Test  // вводим имя или номер для удаления, которого нет в файле. Ошибка.
+    public void Remove() throws Exception {
+        final NoteBook  c = new NoteBook ();
+
+
+    }
+
+    @Test  // поиск по имени или номеру. Такого контакта нет. Ошибка.
+    public void Search() throws Exception {
+        final NoteBook  c = new NoteBook ();
+
+
+    }
+
+    @Test  // Нет такой команды. Ошибка.
+    public void Commander() throws Exception {
+        final NoteBook  c = new NoteBook ();
+
+
+    }
+}
---
+++ NoteBook.java
@@ -0,0 +1,200 @@
+import java.io.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Анна
+ * Date: 05.04.13
+ * Time: 7:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class NoteBook {
+    private final File f;
+
+    //создавание файла
+    public NoteBook () throws IOException {
+        // File f;
+        f = new File("myfile.txt");
+        if (!f.exists()) {
+            f.createNewFile();
+            System.out.println("New file \"myfile.txt\" has been created to the current directory");
+        }
+    }
+
+    //добавление данных в файл f
+    public void Add() throws Exception {
+        String name;
+        BufferedReader reader;
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("What is your name? ");
+        name = reader.readLine();
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            if (line.trim().equals(name)) {
+                System.out.print(name + " is already created. Rename your contact");
+                Add();
+                throw new Exception(name + " is already created. Rename your contact");
+            }
+            line = br.readLine();
+        }
+
+        String content = "";
+        String lineSeparator = System.getProperty("line.separator");   // перевод на след. строку в файле
+        content += lineSeparator + name;
+        FileWriter fw = new FileWriter(f, true);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(content);
+
+        String phone;
+        System.out.print("Please, enter your phone ");
+        phone = reader.readLine();
+        String content2 = "";
+        content2 += lineSeparator + phone;
+        FileWriter fw2 = new FileWriter(f, true);
+        BufferedWriter bw2 = new BufferedWriter(fw);
+        bw.write(content2);
+        bw.close();
+        // fw.close();
+        bw2.close();
+        //fw2.close();
+
+
+    }
+
+    // удаление выбранных данных
+    public void remove() throws IOException {
+        String nameToRemove;
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the contact to remove -> ");
+        nameToRemove = br1.readLine();
+
+        try {
+            //Construct the new file that will later be renamed to the original filename.
+            File tempFile = new File(f.getAbsolutePath() + ".tmp");
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            PrintWriter pw = new PrintWriter(new FileWriter(tempFile));
+            String line = null;
+            //Read from the original file and write to the new
+            //unless content matches data to be removed.
+            while ((line = br.readLine()) != null) {
+                if (!line.trim().equals(nameToRemove)) {
+
+                    pw.println(line);
+                    pw.flush();
+                }
+                if (line.trim().equals(nameToRemove)) {
+                    line = br.readLine();
+                }
+            }
+            pw.close();
+            br.close();
+
+            //Delete the original file
+            if (!f.delete()) {
+                System.out.println("Could not delete file");
+                return;
+            }
+
+            //Rename the new file to the filename the original file had.
+            if (!tempFile.renameTo(f))
+                System.out.println("Could not rename file");
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    //поиск номера по имени. Метод возвращает номер, записанный в файле на строчку ниже, чем имя.
+    public String searchByName() throws IOException {
+        String name;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please, enter the name to search -> ");
+        name = br.readLine();
+        try {
+            BufferedReader br2 = new BufferedReader(new FileReader(f));
+            String line = null;
+            while ((line = br2.readLine()) != null) {
+
+                if (line.trim().equals(name)) {
+                    return br2.readLine();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public String searchByPhone() throws IOException {
+        String phone;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Please, enter the phone to search -> ");
+        phone = br.readLine();
+        try {
+            BufferedReader br2 = new BufferedReader(new FileReader(f));
+            String line = null;
+            String line2 = null;
+            while ((line2 = br2.readLine()) != null) {
+                if (line2.trim().equals(phone)) {
+                    return line;
+                }
+                line = line2;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
+
+    public void Open() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line + " ");
+        }
+    }
+
+    public void Commander() throws Exception {
+        final NoteBook  c = new NoteBook ();
+        String command;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the command -> ");
+        command = br.readLine();
+        if (command.equals("open")) {
+            c.Open();
+        }
+        if (command.equals("searchByName")) {
+            System.out.println(c.searchByName());
+        }
+        if (command.equals("searchByPhone")) {
+            System.out.println(c.searchByPhone());
+        }
+        if (command.equals("remove")) {
+            c.remove();
+        }
+        if (command.equals("add")) {
+            c.Add();
+        }
+        if (!command.equals("add") && !command.equals("remove") && !command.equals("open") && !command.equals("searchByName") && !command.equals("searchByPhone")) {
+            System.out.println("your command is impossible. " +
+                    "Possible commands: open, searchByName, searchByPhone, remove or add. Please,enter the command again -> ");
+            c.Commander();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        final NoteBook  c = new NoteBook ();
+        //c.Commander();
+        //c.Commander();
+        c.Add();
+
+    }
+}
